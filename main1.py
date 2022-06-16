ac19e65b1b3ddf5b39bc0145396fb578addb443c
AV <92280537+adivaste@users.noreply.github.com>
2022-06-16 23:40:16 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -7,15 +7,28 @@ from mega import Mega
 import time
 
 def startNGROKServer():
+
     print("NGROK")
-    # os.system("wget -q -c -nc https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip")
-    # os.system("unzip -qq -n ngrok-stable-linux-amd64.zip")
+    
+    # Searching for "ngrok" binary file
+    currFolderFiles = os.listdir()
+    if "ngrok" not in currFolderFiles:
+        os.system("wget -q -c -nc https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip")
+        os.system("unzip -qq -n ngrok-stable-linux-amd64.zip")
+        
+    # Providing the execution permission to "ngrok"
     os.system("chmod 777 ngrok")
+    
+    # Setting up authentication token
     os.system("./ngrok authtoken 2AZ6Q4rkOlX4Lk8Wyl2p6LeSzm5_86zUyKaEBTBdMDf9nrQjV")
+    
+    # Starting TCP using NGROK on PORT:5050 
     os.system("./ngrok tcp 5050")
 
 def storeResult():
-    time.sleep(0.3)
+    
+    # Waiting for some time to start NGROK server 
+    time.sleep(0.5)
     print("Result are storing...")
 
     host = ""
@@ -29,9 +42,10 @@ def storeResult():
             f.write(f"Port : {port}")
 
         m = Mega()
-        usr = m.login("coding01ninjas@yopmail.com","Bhaibhai45")
+        usr = m.login("online45notepad@yopmail.com","Online45note")
         usr.upload("host_port.txt")
-        print("Uploaded..................")
+        print("Uploaded Host_port.txt ...")
 
+# Creating the two threads
 x = threading.Thread(target=startNGROKServer)
 y = threading.Thread(target=storeResult)
