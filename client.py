52131d46c63acd9ede59f30fb795f4822dd6b594
Aditya Vaste <adi45v@gmail.com>
2022-06-17 08:05:32 +0530

    Upadted Port and Host

--- client.py
+++ client.py
@@ -2,8 +2,8 @@ import json
 import socket
 import sys
 
-IP = "0.tcp.ngrok.io"	#sys.argv[1]
-PORT = 18978    #int(sys.argv[2])
+IP = "8.tcp.ngrok.io"	#sys.argv[1]
+PORT = 17614    #int(sys.argv[2])
 ADDR = (IP, PORT)
 FORMAT = "utf-8"
 SIZE = 1024
